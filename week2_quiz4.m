9f7d8ac4557e614f6b018039477da71c381485a0
Julio CN Souza <juliocnsouzadev@gmail.com>
2017-03-05 23:46:26 -0300

    linear regression exercise 1

---
+++ week2_quiz4.m
@@ -0,0 +1,22 @@
+%% Clear and Close Figures
+clear ; close all; clc
+
+examScores = [89,	7921,	96; 72,	5184,	74;94,	8836,	87;69,	4761,	78]
+
+
+meanScores = mean(examScores)
+
+maxScores = max(examScores)
+
+minScores = min(examScores)
+
+xCol = 2
+xRow = 2
+
+columnRange = maxScores(xCol) - minScores(xCol)
+
+currentFeatureValue = examScores(xRow, xCol)
+
+normalizedFeatureValue = (currentFeatureValue - meanScores(xCol)) / columnRange
+
+disp(normalizedFeatureValue)
